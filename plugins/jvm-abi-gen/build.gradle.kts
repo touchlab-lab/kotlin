20b1fe59651cce67fcf6de0b4be3e897713aa842
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2022-06-15 15:17:59 +0200

    ~ rollback after new version of kotlinx-metadata is available

--- plugins/jvm-abi-gen/build.gradle.kts
+++ plugins/jvm-abi-gen/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     // Note that kotlinx-metadata-jvm already includes kotlinx-metadata, core:metadata, core:metadata.jvm,
     // and protobuf-lite, so we only need to include kotlinx-metadata-jvm in the shadow jar.
     compileOnly(project(":kotlinx-metadata"))
-    shadows(commonDependency("org.jetbrains.kotlinx:kotlinx-metadata-jvm"))
+    shadows(project(":kotlinx-metadata-jvm"))
 
     compileOnly(intellijCore())
     compileOnly(commonDependency("org.jetbrains.intellij.deps:asm-all"))
